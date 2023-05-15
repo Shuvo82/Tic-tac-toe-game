a9c8a39ca20dc3429ed05d3d673206f8f32f01ef
Shuvo <Sohorafuzzamanshuvo@gmail.com>
2023-05-15 21:43:06 +0600

    Final version 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -99,6 +99,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
       home: Scaffold(
         appBar: AppBar(
